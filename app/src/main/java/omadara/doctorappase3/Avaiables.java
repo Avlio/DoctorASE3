b3540f12514a5a265f987e35d066fdfe78e6ef5d
DESKTOP-A3M73V7\plzdo <mai17022@uom.edu.gr>
2017-01-22 17:48:19 +0200

    Activities

---
+++ app/src/main/java/omadara/doctorappase3/Avaiables.java
@@ -0,0 +1,8 @@
+package omadara.doctorappase3;
+
+/**
+ * Created by plzdo on 22/1/2017.
+ */
+
+public interface Avaiables {
+}
---
+++ app/src/main/java/omadara/doctorappase3/LoginActivity.java
@@ -0,0 +1,10 @@
+package omadara.doctorappase3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+    }
+}
